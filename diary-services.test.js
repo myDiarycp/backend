af6b7a7834155f885b4f4c2e41c94b42c5d77771
JeremyKozlowski <jjkozlow@calpoly.edu>
2022-11-28 16:15:04 -0800

    Reordering and revising tests to not depend on entries... 1 fails

--- diary-services.test.js
+++ diary-services.test.js
@@ -1,19 +1,5 @@
 const userServices = require('./models/diary-services.js');
 
-test('Testing getUsers -- success', async () => {
-    const result = await userServices.getUsers(); 
-
-    expect(result[0].name).toBe("Yoel");
-    expect(result[0].subject).toBe("9a5c1f3b-51a4-4baa-a9dd-4dc3a5c665b0")
-  });
-
-test('Testing findUserBySubject -- success', async () => {
-  const result = await userServices.findUserBySubject("9a5c1f3b-51a4-4baa-a9dd-4dc3a5c665b0"); 
-
-  expect(result[0].name).toBe("Yoel");
-  expect(result[0].subject).toBe("9a5c1f3b-51a4-4baa-a9dd-4dc3a5c665b0")
-});
-
 test('Testing addUser -- success', async () => {
   user ={ 
     _id: 'testsubject',
@@ -30,6 +16,22 @@ test('Testing addUser -- success', async () => {
 
   expect(result[0].subject).toBe("testsubject")
 
+});
+
+test('Testing getUsers -- success', async () => {
+    const result = await userServices.getUsers(); 
+
+    console.log(result);
+
+    expect(result.reverse[0].name).toBe("testname");
+    expect(result.reverse[0].subject).toBe("testsubject")
+  });
+
+test('Testing findUserBySubject -- success', async () => {
+  const result = await userServices.findUserBySubject("testsubject"); 
+
+  expect(result[0].name).toBe("testname");
+  expect(result[0].subject).toBe("testsubject")
 });
 
 
@@ -53,28 +55,22 @@ test('Testing findUserBySubjectAndUpdate -- success', async () => {
   }
 
   const put = await userServices.findUserBySubjectAndUpdate("testsubject", user);
-
-  //console.log(put);
-
   const result = await userServices.findUserBySubject("testsubject");
 
-expected = { 
-"date": "10-11-2022", 
-"rating": 10, 
-"text": "I had a great day!", 
-"title": "My First Entry"}
+  expected = { 
+  "date": "10-11-2022", 
+  "rating": 10, 
+  "text": "I had a great day!", 
+  "title": "My First Entry"}
 
-  expect(result[0].diary.get('10-11-2022')).toMatchObject(expected);
+    expect(result[0].diary.get('10-11-2022')).toMatchObject(expected);
 
-});
+  });
 
 test('Testing deleteUser -- success', async () => {
 
   const result = await userServices.findUserBySubject("testsubject");
-
   const del = await userServices.deleteUser(result[0].id);
-
-
   const afterResult = await userServices.findUserBySubject("testsubject")
 
   expect(afterResult).toEqual([]);
